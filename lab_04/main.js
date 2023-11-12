1f0206526cfdf4d226e2c5c51e166bb041cc78e5
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-11-12 16:51:28 +0100

    Remove console logs

--- lab_04/main.js
+++ lab_04/main.js
@@ -26,7 +26,6 @@ class wordGenerator {
     
         getRandomWord() {
             var index = Math.floor(Math.random() * this.wordList.length);
-            console.log("Random word: " + this.wordList[index]);
             return this.wordList[index];
         }
     
@@ -35,8 +34,6 @@ class wordGenerator {
 class Game {
 
     constructor() {
-
-        console.log("Game constructor called")
 
         // Game variables
         this.initailizeGame();
@@ -59,13 +56,11 @@ class Game {
     resetGame() {
         this.initailizeGame();
 
-        console.log("Resetting words");
         this.wordDisplayer.clearDisplayedWord("goal_word");
         this.wordDisplayer.clearDisplayedWord("current_word");
         this.wordDisplayer.clearDisplayedWord("score");
         this.wordDisplayer.clearDisplayedWord("end-game");
 
-        console.log("Resetting intervals");
         this.clearIntervals();
     }
 
@@ -91,16 +86,12 @@ class Game {
     }
 
     startGame() {
-
-        console.log("Game started");
     
         // get the goal word
-        console.log("Getting goal word");
         this.setWordGoal(this.wordGenerator.getRandomWord());
         this.wordDisplayer.displayWord(this.getWordGoal(), "goal_word");
     
         // display score
-        console.log("Displaying score");
         this.wordDisplayer.displayWord(this.points, "score");
 
         this.setRandomWordInterval();
@@ -110,9 +101,6 @@ class Game {
     
     stopGame() {
 
-        console.log("Stopping game");
-
-        console.log("Clearing intervals");
         this.clearIntervals();
 
         if (this.stopClickedFlag && !this.shouldGameStop()) {
@@ -132,33 +120,27 @@ class Game {
     catchWord() {
     
         if (this.shouldGameStop()) {
-            console.log("Game stopped, can't catch words!");
             return;
         }
     
         if (this.compareWords()) {
-            console.log("Words match!");
             this.addPoints();
         } else {
-            console.log("Words don't match!");
             this.removePoints();
         }
     }
     
     addPoints() {
-        console.log("Points added");
         this.points++;
         this.wordDisplayer.displayWord(this.points, "score");
     }
     
     removePoints() {
-        console.log("Points removed");
         this.points--;
         this.wordDisplayer.displayWord(this.points, "score");
     }
     
     compareWords() {
-        console.log("Comparing words: " + this.getCurrentWord() + " and " + this.getWordGoal() );
         return (this.getCurrentWord().localeCompare(this.getWordGoal()) === 0);
     }
     
@@ -167,14 +149,10 @@ class Game {
     }
 
     setRandomWordInterval() {
-        console.log("Setting random word interval");
     
         this.randomWordInterval = setInterval(() => {
-    
-            console.log("Inside random word interval");
-    
+        
             if (!this.shouldGameStop()) {
-                console.log("shouldGameStop evaluated to false, displaying random word ");
                 this.setCurrentWord(this.wordGenerator.getRandomWord());
                 this.wordDisplayer.displayWord(this.getCurrentWord(), "current_word");
             } else {
@@ -187,7 +165,6 @@ class Game {
     setGameOverInterval() {
         this.gameOverInterval = setInterval(function() {
             if (this.shouldGameStop()) {
-                console.log("Game over");
                 this.stopGame();
             }
         }, 100);
@@ -198,23 +175,19 @@ class Game {
 let game;
 
 function startGameHandler() {
-    console.log("Creating a new game");
     game = new Game();
     game.startGame();
 }
 
 function catchWordHandler() {
-    console.log("Catch clicked!");
     game.catchWord();
 }
 
 function stopGameHandler() {
-    console.log("Stop clicked!");
     game.stopClickedFlag = true;
     game.stopGame();
 }
 
 function resetGameHandler() {
-    console.log("Reset clicked!");
     game.resetGame();
 }
